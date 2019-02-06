4b536ae5720a4a6eb38df93f0e3e27593ddafcd8
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-05 22:59:48 -0800

    Update string-helpers.c

--- string-helpers.c
+++ string-helpers.c
@@ -131,7 +131,7 @@ int is_valid_identifier(char* str) {
         }
         n++;
     }
-    if (str[sizeof (str) - 1] != '\0') {
+    if (str[sizeof (str) - 1] == '\0') {
         return 0;
     }
     return 1;
