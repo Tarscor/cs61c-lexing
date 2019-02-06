e5f4f819b12d8b90723f6720a7768bb97d501749
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-05 23:04:10 -0800

    Update string-helpers.c

--- string-helpers.c
+++ string-helpers.c
@@ -126,6 +126,7 @@ int is_valid_identifier(char* str) {
   /* YOUR CODE HERE */
     int n = 0;
     while (n < sizeof (str)) {
+        printf("%c", str[n]);
         if (!is_identifier_component(str[n])) {
            return 0;
         }
