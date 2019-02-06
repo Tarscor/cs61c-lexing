c8fead91f40d0569054830ed30a6b3eba25d2877
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-06 09:01:55 -0800

    Update test.c

--- tests/cunit/test.c
+++ tests/cunit/test.c
@@ -225,6 +225,7 @@ void testISVALIDID_All(void) {
 void testSTRCONCAT_TwoStr(void) {
   char* array[2] = { "Hel", "lo" };
   str_concat(array, 2);
+  printf("hi");
 }
 void testSTRCONCAT_SingleChar(void) {
   char* array[5] = { "H", "e", "l", "l", "o" };
