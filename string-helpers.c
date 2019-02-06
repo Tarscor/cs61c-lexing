29f4bee6aae9d9f38d1056497a77b966c8742e1b
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-06 07:48:42 -0800

    Update string-helpers.c

--- string-helpers.c
+++ string-helpers.c
@@ -154,12 +154,11 @@ char* str_concat(char** strings, size_t count) {
     int index = 0;
     for (int i = 0; i < count; i++) {
         for (int j = 0; j < strlen (strings[i]); j++) {
-            printf("%c", strings[i][j]);
             concat[index] = strings[i][j];
             index++;
         }
     }
     strings[index] = '\0';
-    char* pointer = concat;
+    char pointer[] = concat;
     return pointer;
 }
