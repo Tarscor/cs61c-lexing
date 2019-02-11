1d441b1d5a6139603abc80791c4a0ff8ba7afd10
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-10 22:33:20 -0800

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -387,7 +387,7 @@ size_t SelectToken(char* buffer,
       if (buffer[size_read + 1] == '\n') {
         t = create_token(filename);
         t->linenum = *linenum;
-        t->data.error = (int)buffer[size_read];
+        t->data.error = 2;
         t->type = TOKEN_ERR;
         size_read++;
       } else {
