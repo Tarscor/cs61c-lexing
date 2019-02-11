4e3ac1e978ba0fec83db83cb80ad7ef42f8a8440
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-11 14:04:04 -0800

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -507,7 +507,6 @@ size_t SelectToken(char* buffer,
           token_contents[j] = buffer[size_read + j];
         }
         token_contents[id_len] = '\0';
-        printf("%c", token_contents[0]);
         enum TokenType type = check_keyword(token_contents);
         if (type != TOKEN_ERR) {
           t = create_token(filename);
