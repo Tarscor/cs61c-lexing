52aefb000629b66af1038c1dc34e1fc477551c02
Tarscor <38743505+Tarscor@users.noreply.github.com>
2019-02-11 18:01:35 -0800

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -452,7 +452,6 @@ size_t SelectToken(char* buffer,
     }
   } else if (is_digit(
                  buffer[size_read])) {  // positive integers and some errors
-    printf("hi");
     size_t int_len = 1;
     int search = 1;
     while (size_read + int_len < size && search) {
